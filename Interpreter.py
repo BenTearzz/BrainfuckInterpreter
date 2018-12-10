dbaa5ee2be03fd488ce01d6aa4c24bbbeca47b71
BenTearzz <benjamin.stigsen@gmail.com>
2018-12-11 00:32:06 +0100

    Fixed debug mode
    Fixed arrow placement when using the debug character.

--- Interpreter.py
+++ Interpreter.py
@@ -3,151 +3,161 @@ from readchar import readchar
 from sys import argv
 
 class BrainfuckInterpreter():
-    def __init__(self):
+	def __init__(self):
 
-        # Settings & Setup
-        self.cells = [0] # Create a cell
-        self.index = 0   # Start at cell 0
+		# Settings & Setup
+		self.cells = [0] # Create a cell
+		self.index = 0   # Start at cell 0
 
-        self.shifting   = {">": 1, "<":-1}  # Shortcut for shifting cells
-        self.increment  = {"+": 1, "-":-1}  # Shortcut for incrementing & decrementing cells
+		self.shifting   = {">": 1, "<":-1}  # Shortcut for shifting cells
+		self.increment  = {"+": 1, "-":-1}  # Shortcut for incrementing & decrementing cells
 
-        self.cell_value_max = 255   #255 is the max ASCII value
+		self.cell_value_max = 255   #255 is the max ASCII value
 
-        code = None
-        if len(argv[1:]) > 0:       # Check if an argument was passed
-            file = argv[1].strip()  # Set file to the first argument
-            try:
-                f = open(file, "r") # Open file for reading
-                code = f.read()     # Read file and save the content in variable "code"
-            except Exception as err:
-                print("Error: ", str(err))
-                raise SystemExit
+		code = None
+		if len(argv[1:]) > 0:       # Check if an argument was passed
+			file = argv[1].strip()  # Set file to the first argument
+			try:
+				f = open(file, "r") # Open file for reading
+				code = f.read()     # Read file and save the content in variable "code"
+			except Exception as err:
+				print("Error: ", str(err))
+				raise SystemExit
 
-        if code == None:
-            code = input("Brainfuck code: ")
+		if code == None:
+			code = input("Brainfuck code: ")
 
-        if code.strip() != "":
-            self.interpret(code.strip())
-        else:
-            print("No code")
-            raise SystemExit
+		if code.strip() != "":
+			self.interpret(code.strip())
+		else:
+			print("No code")
+			raise SystemExit
 
-    def interpret(self, code):
-        show_cells_enabled = True
-        show_cells_character = "!"
-        c = 0   # Start at character 0
+	def interpret(self, code):
+		show_cells_enabled = True
+		show_cells_character = "!"
+		c = 0   # Start at character 0
 
-        while c < len(code):
-            char = code[c]  # Character / Symbol is the currentcharacter in our code
+		while c < len(code):
+			char = code[c]  # Character / Symbol is the currentcharacter in our code
 
-            # Show Cells
-            if char == show_cells_character and show_cells_enabled: # If character == "show_cells_character" and "show_cells_enabled" is True
-                cells_str = ", ".join(str(i) for i in self.cells)
-                print(self.cells)
+			# Show Cells
+			if char == show_cells_character and show_cells_enabled: # If character == "show_cells_character" and "show_cells_enabled" is True
+				cells_str = ", ".join(str(i) for i in self.cells)
+				print(self.cells)
 
-                i = 0
-                for pos, i_ in enumerate(cells_str): # Iterate through every single character
-                    if i_ == ",":
-                        i += 1
+				i = 0
+				for pos, i_ in enumerate(cells_str): # Iterate through every single character
+					if i_ == ",":
+						i += 1
 
-                    if i >= self.index:
-                        if pos == 0:
-                            pos = -2
+					if i >= self.index:
+						if pos == 0:
+							pos = -2
 
-                        print(" " * (pos + 2) + " ^")
-                        break
+						break
 
-            # Shifting
-            if char == ">" or char == "<":
-                self.cell_shifting(char)
+				width = 2
 
-            # Incrementing & Decrementing
-            if char == "+" or char == "-":
-                self.cell_increment(char)
+				for i in range(len(self.cells)-i):
+					if self.cells[i] > 9:
+						width += 1
 
-            # Looping
-            if char == "[":
-                c = c + self.cell_loop(char, code[c:])
-            elif char == "]":
-                c = c + self.cell_loop(char, code[:c])
+					if self.cells[i] > 99:
+						width += 1
 
-            # Input & Output
-            if char == ",":
-                self.cells[self.index] = ord(readchar())
-                print(chr(self.cells[self.index]))
-            elif char == ".":
-                print(chr(self.cells[self.index]), end="") # end="" to print on one-line (default: "\n")
+				print(" " * (pos + width) + " ^")
 
-            #print(self.cells, "["+char+"]") # Uncomment if you want to see what happens step-by-step
-            c += 1
+			# Shifting
+			if char == ">" or char == "<":
+				self.cell_shifting(char)
 
-        #print("\n" + str(self.cells))
-        input() # So that it doesn't just close when done
+			# Incrementing & Decrementing
+			if char == "+" or char == "-":
+				self.cell_increment(char)
 
-    # Shifting
-    def cell_shifting(self, char):
-        self.index += self.shifting[char]
+			# Looping
+			if char == "[":
+				c = c + self.cell_loop(char, code[c:])
+			elif char == "]":
+				c = c + self.cell_loop(char, code[:c])
 
-        if self.index < 0:                  # If the cell index is below 0
-            self.index = 0                  # Make the cell index 0
+			# Input & Output
+			if char == ",":
+				self.cells[self.index] = ord(readchar())
+				print(chr(self.cells[self.index]))
+			elif char == ".":
+				print(chr(self.cells[self.index]), end="") # end="" to print on one-line (default: "\n")
 
-        if self.index > len(self.cells)-1:  # If the cell index is above the current amount of cells
-            self.cells.append(0)            # Create a new cell.
+			#print(self.cells, "["+char+"]") # Uncomment if you want to see what happens step-by-step
+			c += 1
 
-    # Incrementing & Decrementing
-    def cell_increment(self, char):
-        self.cells[self.index] += self.increment[char]
+		#print("\n" + str(self.cells))
+		input() # So that it doesn't just close when done
 
-        if self.cells[self.index] < 0:                      # If the cell value is below 0
-            self.cells[self.index] = self.cell_value_max    # Make the cell value "cell_value_max" (Default: 255)
+	# Shifting
+	def cell_shifting(self, char):
+		self.index += self.shifting[char]
 
-        if self.cells[self.index] > self.cell_value_max:    # If the cell value is above "cell_value_max" (Default: 255)
-            self.cells[self.index] = 0                      # Make the cell value 0
+		if self.index < 0:                  # If the cell index is below 0
+			self.index = 0                  # Make the cell index 0
 
-    # Looping
-    def cell_loop(self, char, code):
-        match = 0
-        i = 0
+		if self.index > len(self.cells)-1:  # If the cell index is above the current amount of cells
+			self.cells.append(0)            # Create a new cell.
 
-        if char == "[":
+	# Incrementing & Decrementing
+	def cell_increment(self, char):
+		self.cells[self.index] += self.increment[char]
 
-            if self.cells[self.index] != 0: # If our current cell is NOT 0
-                return 0                    # Return 0 (it does c += 1 in the "interpret" function)
-                                            # Stays at current cell, "c += 1" will move to the instruction inside [ ]
+		if self.cells[self.index] < 0:                      # If the cell value is below 0
+			self.cells[self.index] = self.cell_value_max    # Make the cell value "cell_value_max" (Default: 255)
 
-            while match != 1:
-                if i == len(code):
-                    return 0
+		if self.cells[self.index] > self.cell_value_max:    # If the cell value is above "cell_value_max" (Default: 255)
+			self.cells[self.index] = 0                      # Make the cell value 0
 
-                if code[i] == "[":
-                    match -= 1
+	# Looping
+	def cell_loop(self, char, code):
+		match = 0
+		i = 0
 
-                if code[i] == "]":
-                    match += 1
+		if char == "[":
 
-                i += 1
+			if self.cells[self.index] != 0: # If our current cell is NOT 0
+				return 0                    # Return 0 (it does c += 1 in the "interpret" function)
+											# Stays at current cell, "c += 1" will move to the instruction inside [ ]
 
-            return i
+			while match != 1:
+				if i == len(code):
+					return 0
 
-        if char == "]":
+				if code[i] == "[":
+					match -= 1
 
-            if self.cells[self.index] == 0: # If our current cell is 0
-                return 0                    # Return 0 (it does c += 1 in the "interpret" function)
-                                            # Stays at current cell, "c += 1" will move to the instruction outside of the ]
-            while match != 1:
-                if i == -len(code):         # If we're at the last cell and there's no match
-                    return 0                # Stays at current cell, "c += 1" will move to the instruction outside of the ]
+				if code[i] == "]":
+					match += 1
 
-                if code[i] == "]":
-                    match -= 1
+				i += 1
 
-                if code[i] == "[":
-                    match += 1
+			return i
 
-                i -= 1
+		if char == "]":
 
-            return i
+			if self.cells[self.index] == 0: # If our current cell is 0
+				return 0                    # Return 0 (it does c += 1 in the "interpret" function)
+											# Stays at current cell, "c += 1" will move to the instruction outside of the ]
+			while match != 1:
+				if i == -len(code):         # If we're at the last cell and there's no match
+					return 0                # Stays at current cell, "c += 1" will move to the instruction outside of the ]
+
+				if code[i] == "]":
+					match -= 1
+
+				if code[i] == "[":
+					match += 1
+
+				i -= 1
+
+			return i
 
 bf = BrainfuckInterpreter
 bf()
